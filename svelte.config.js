28a5f2781ca7e1598231737141dca43f16c51c24
Aditya Rizqi Ardhana <ardhanajames777@gmail.com>
2024-01-14 13:28:34 +0700

    fix: change adapter svelte

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-// import cloudFlare from '@sveltejs/adapter-cloudflare';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,10 +7,7 @@ const config = {
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
-	adapter: adapter({
-		edge: false,
-		split: true
-	})
+	adapter: adapter()
 };
 
 export default config;

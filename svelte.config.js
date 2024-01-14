82eedc960464b6c17bc5780bfa2c17569ceeba69
Aditya Rizqi Ardhana <ardhanajames777@gmail.com>
2024-01-14 13:03:57 +0700

    feat: add configuration deplot to netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
-import cloudFlare from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+// import cloudFlare from '@sveltejs/adapter-cloudflare';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,15 +8,10 @@ const config = {
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
-	kit: {
-		adapter: cloudFlare({
-			// REF https://github.com/sveltejs/kit/blob/fd6eb9b152001a537f4277a9e597aa24405a51af/documentation/docs/25-build-and-deploy/60-adapter-cloudflare.md#usage
-			routes: {
-				include: ['/*'],
-				exclude: ['<all>']
-			}
-		}),
-	}
+	adapter: adapter({
+		edge: false,
+		split: true
+	})
 };
 
 export default config;
